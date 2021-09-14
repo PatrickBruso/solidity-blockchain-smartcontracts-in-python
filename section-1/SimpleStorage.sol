b50995d9df0d3dd47000e7ccb4e6aebf0f0c7459
Patrick Bruso <pkbruso@gmail.com>
2021-09-13 17:53:09 -0700

    added retrieve function and struct People

--- section-1/SimpleStorage.sol
+++ section-1/SimpleStorage.sol
@@ -16,9 +16,29 @@ contract SimpleStorage {
     removed variable types per course instruction
     */
 
+     struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    
+    People public person = People({favoriteNumber: 6, name: "Patrick"});
+    
+
     // Function that takes in an unisgned int and assigns it to variable favoriteNumber
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
+
+    // view function reads (but does not write) off the blockchain
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+    
+    /*
+    // pure function allows for math computations
+    function retrieveTwo(uint256 favoriteNumber) public pure {  // no returns mean it does not change the state of the blockchain
+        favoriteNumber + favoriteNumber;
+    }
+    */
 
 }
